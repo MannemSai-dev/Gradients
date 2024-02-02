86772907fdaf92ddbdb6ddba0a41af6dc0e86281
Mannem Sai <mannemsai161@gmail.com>
2024-02-02 20:26:05 +0530

    updated All

---
+++ script.js
@@ -0,0 +1,14 @@
+var color1 = document.getElementById("color1")
+var color2 = document.getElementById("color2")
+var bg = document.getElementById("bg")
+let genButton = document.querySelector("#generate")
+let text = document.getElementById("text")
+
+function updateBackground() {
+    bg.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+    text.textContent = bg.style.background+";";
+  }
+  
+  color1.addEventListener('input', updateBackground);
+  color2.addEventListener('input', updateBackground);
+  
